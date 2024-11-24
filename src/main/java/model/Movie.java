deff119785b033cb1c57bddaf6293a020d7e37da
Syed Rastin Nuri Reza <srnr@mailbox.unideb.hu>
2024-11-24 22:19:20 +0100

    Restoring everything after there was an accidental override of the repo

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,4 +1,57 @@
 package model;
 
 public class Movie {
+    private int id;
+    private String title;
+    private String genre;
+    private int duration; // in minutes
+    private String rating;
+
+    public Movie(int id, String title, String genre, int duration, String rating) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.duration = duration;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,28 @@
 package model;
 
 public class User {
+
+    private String name;
+    private boolean isStudentOrRetiree;
+
+    public User(String name, boolean isStudentOrRetiree) {
+        this.name = name;
+        this.isStudentOrRetiree = isStudentOrRetiree;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isStudentOrRetiree() {
+        return isStudentOrRetiree;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentOrRetiree(boolean studentOrRetiree) {
+        isStudentOrRetiree = studentOrRetiree;
+    }
 }
