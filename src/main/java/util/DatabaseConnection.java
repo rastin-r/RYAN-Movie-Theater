f356f5f2b2b261545a094e9acd0b2b4f04288888
Syed Rastin Nuri Reza <srnr@mailbox.unideb.hu>
2024-12-08 16:00:38 +0100

    Add: DatabaseConnection

--- src/main/java/util/DatabaseConnection.java
+++ src/main/java/util/DatabaseConnection.java
@@ -1,4 +1,23 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Provides a centralized method to retrieve a database connection using JDBC.
+ */
 public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:mysql://sdm24-db.cryoawqmgfhw.eu-north-1.rds.amazonaws.com:3306/sdm_db?user=admin";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "RYAN-sdm24!";
+
+    /**
+     * Creates and returns a connection to the database.
+     * @return a Connection object to the configured database
+     * @throws SQLException if a database access error occurs or the url is null
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
 }
