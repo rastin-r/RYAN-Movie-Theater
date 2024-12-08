d46fda8a7998500336afe080c342d43c9c8ce8d6
hmwassi98 <hmwassi1998@gmail.com>
2024-12-08 16:35:11 +0100

    Update: Reservation

--- src/main/java/model/Reservation.java
+++ src/main/java/model/Reservation.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Reservation {
+    private int reservationId;
+    private int userId;
+    private int movieId;
+    private int showtimeId;
+    private String[] seatNumbers;
+    private boolean isStudentOrRetiree;
+
+    public Reservation(int reservationId, int userId, int movieId, int showtimeId, String[] seatNumbers, boolean isStudentOrRetiree) {
+        this.reservationId = reservationId;
+        this.userId = userId;
+        this.movieId = movieId;
+        this.showtimeId = showtimeId;
+        this.seatNumbers = seatNumbers;
+        this.isStudentOrRetiree = isStudentOrRetiree;
+    }
+
+    // Getters and Setters
+    public int getReservationId() { return reservationId; }
+    public int getUserId() { return userId; }
+    public int getMovieId() { return movieId; }
+    public int getShowtimeId() { return showtimeId; }
+    public String[] getSeatNumbers() { return seatNumbers; }
+    public boolean isStudentOrRetiree() { return isStudentOrRetiree; }
 }
